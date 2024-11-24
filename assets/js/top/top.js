9f2344c19a06f6034a46f9bcb5a6fd207195e9f3
tacccccc21 <tacccccc21@gmail.com>
2024-11-24 17:59:25 +0900

    ヘッダーフッター

---
+++ assets/js/top/top.js
@@ -0,0 +1,18 @@
+    // オプションを渡す
+    var rellax = new Rellax('.rellax', {
+        speed: 8,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+      });
+
+      var rellax = new Rellax('.rellax-2', {
+        speed: 12,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+      });
